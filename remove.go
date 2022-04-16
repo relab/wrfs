a641cd350078325bf268e5c1107b48d09fad23d6
John Ingve Olsen <johningveolsen@gmail.com>
2022-04-16 10:20:20 +0200

    RemoveAll: use path.Join instead of filepath.Join
    filepath.Join uses backslashes on windows, but we always want forward
    slashes

--- remove.go
+++ remove.go
@@ -1,7 +1,7 @@
 package wrfs
 
 import (
-	"path/filepath"
+	"path"
 )
 
 // RemoveFS is a file system that supports the Remove function.
@@ -29,35 +29,35 @@ type RemoveAllFS interface {
 }
 
 // RemoveAll removes path and any children it contains.
-func RemoveAll(fsys FS, path string) error {
+func RemoveAll(fsys FS, removePath string) error {
 	if fsys, ok := fsys.(RemoveAllFS); ok {
-		return fsys.RemoveAll(path)
+		return fsys.RemoveAll(removePath)
 	}
 
-	fi, err := Stat(fsys, path)
+	fi, err := Stat(fsys, removePath)
 	if err != nil {
 		return err
 	}
 
 	// Check if we are removing a file or a directory.
 	if !fi.IsDir() {
-		return Remove(fsys, path)
+		return Remove(fsys, removePath)
 	}
 
-	files, err := ReadDir(fsys, path)
+	files, err := ReadDir(fsys, removePath)
 	if err != nil {
 		return err
 	}
 
 	for _, fi := range files {
 		if fi.IsDir() {
-			if err = RemoveAll(fsys, filepath.Join(path, fi.Name())); err != nil {
+			if err = RemoveAll(fsys, path.Join(removePath, fi.Name())); err != nil {
 				return err
 			}
-		} else if err = Remove(fsys, filepath.Join(path, fi.Name())); err != nil {
+		} else if err = Remove(fsys, path.Join(removePath, fi.Name())); err != nil {
 			return err
 		}
 	}
 
-	return Remove(fsys, path)
+	return Remove(fsys, removePath)
 }
